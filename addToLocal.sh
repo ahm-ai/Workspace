423b0e5f5361377d277deb673a9fd77a8561aff6
ahm-ai <user@gmail.com>
2023-01-10 13:54:14 -0600

    Change

---
+++ addToLocal.sh
@@ -0,0 +1,10 @@
+cp ./zshrc/.ASCII_colors  ~/.ASCII_colors &&
+cp ./zshrc/.ffmpegCommands ~/.ffmpegCommands
+
+
+# Add to your .zshrc file:
+# source ~/.ffmpegCommands 
+# source ~/.ASCII_colors 
+
+# Enable latest changes
+source ~/.zshrc
---
+++ bringCurrent.sh
@@ -0,0 +1,3 @@
+cp ~/.ASCII_colors ./zshrc/.ASCII_colors &&
+cp ~/.ffmpegCommands ./zshrc/.ffmpegCommands
+
