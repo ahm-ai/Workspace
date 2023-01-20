005ef0bfb9f3f93daf9dfc06cbdf9bca32b3dae6
ahm-ai <user@gmail.com>
2023-01-20 08:58:05 -0600

    Start

--- enableAliases.sh
+++ enableAliases.sh
@@ -20,13 +20,14 @@ git config --global color.status.added "yellow"
 git config --global color.status.changed "green"
 git config --global color.status.untracked "cyan"
 
-
+# Default Utils
 git config --global alias.st status
 git config --global alias.a add
 git config --global alias.b branch
 git config --global alias.c commit
 git config --global alias.co checkout
 
+# Custom Utils
 git config --global alias.del "branch -D"
 git config --global alias.bl "show-branch --color --list"
 git config --global alias.clean "!git reset --hard"
@@ -45,7 +46,7 @@ git config --global alias.fff "difftool -t vimdiff -y"
 git config --global alias.cms "!git --no-pager log -10 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global alias.cml "!git --no-pager log --graph --pretty=format:'%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(green)(%cr) [%an]' --abbrev-commit -30"
 
-
+# Using FZF
 git config --global alias.cob "!git branch | fzf | xargs -o git checkout"
 git config --global alias.sq "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --squash"
 
