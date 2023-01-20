45afdf922fd04009355e5f6c807b2f6136e3bdc2
ahm-ai <user@gmail.com>
2023-01-19 23:34:47 -0600

    Start

--- enableAliases.sh
+++ enableAliases.sh
@@ -1,10 +1,28 @@
-# cp -f aliases/.gitconfig ~/.gitconfig
-
+git config --global user.email "user@gmail.com"
+git config --global user.name "ahm-ai"
+git config --global filter.lfs.process "git-lfs filter-process"
+git config --global filter.lfs.required true
+git config --global filter.lfs.clean "git-lfs clean -- %f"
+git config --global filter.lfs.smudge "git-lfs smudge -- %f"
+git config --global pull.rebase true
+git config --global pull.ff only
+git config --global color.ui auto
+git config --global color.branch.current "yellow reverse"
+git config --global color.branch.local "yellow"
+git config --global color.branch.remote "green"
+git config --global color.diff.meta "yellow bold"
+git config --global color.diff.frag "magenta bold"
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green bold"
+git config --global color.status.added "yellow"
+git config --global color.status.changed "green"
+git config --global color.status.untracked "cyan"
 git config --global alias.st status
 git config --global alias.a add
 git config --global alias.b branch
 git config --global alias.c commit
 git config --global alias.co checkout
+git config --global alias.del "branch -D"
 git config --global alias.bl "show-branch --color --list"
 git config --global alias.dfm "!git diff --name-status"
 git config --global alias.df "!git diff --name-only HEAD~"
@@ -14,16 +32,10 @@ git config --global alias.sq "!git log -n 50 --pretty=format:'%h %s' --no-merges
 git config --global alias.rb "!git rebase -i --autosquash main"
 git config --global alias.vw "!gh pr view --web"
 git config --global alias.ci "!gh pr checks"
-
-git config --global pager.diff false
-git config --global rebase.autosquash true
-
 git config --global alias.ls "log -1 HEAD --stat --no-pager"
 git config --global alias.fff "difftool -t vimdiff -y"
-git config --global alias.clean "reset --hard"
+git config --global alias.clean "!git reset --hard"
 git config --global alias.cml "!git --no-pager log --graph --pretty=format:'%C(magenta)%h%Creset -%C(red)%d%Creset %s %C(green)(%cr) [%an]' --abbrev-commit -30"
-git config --global alias.del "branch -D"
-git config --global alias.undo "reset HEAD~1 --mixed"
-git config --global alias.res "reset --hard"
+git config --global alias.undo "!git reset HEAD~1 --mixed"
 
 echo "🚀 Aliases enabled"
