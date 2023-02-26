f585a7c9b0dd04734a8c043c2f9fafb97fe444e1
ahm-ai <user@gmail.com>
2023-02-25 22:45:35 -0600

    M

--- .husky/hook_validate.js
+++ .husky/hook_validate.js
@@ -1,30 +1,32 @@
 const fs = require('fs');
 
-const COVERAGE_SUMMARY_FILE = 'coverage/coverage-summary.json';
+const COVERAGE_SUMMARY_FILE = './coverage/coverage-summary.json';
 const MINIMUM_COVERAGE_PERCENTAGE = 80;
 
 const coverageSummary = JSON.parse(fs.readFileSync(COVERAGE_SUMMARY_FILE, 'utf8'));
 console.log(coverageSummary);
 
+if (coverageSummary.total.lines.pct === 'Unknown') {
+//   console.log('Coverage percentage is unknown');
+console.log('\u001b[32m \n ✨ Continue \n \u001b[0m');
+  process.exit(0);
+}
 
 const overallCoveragePercentage = Object.keys(coverageSummary).filter((key)=>{
     if(key !== 'total'){
-        if( coverageSummary[key].lines.pct > 80) return false;
+        if( coverageSummary[key].lines.pct > MINIMUM_COVERAGE_PERCENTAGE) return false;
         return true;
     }
     return false;
 })
 
-// if (overallCoveragePercentage === 'Unknown') {
-//   console.log('Coverage percentage is unknown');
-//   process.exit(0);
-// }
+
 console.log(overallCoveragePercentage);
 
 if (!overallCoveragePercentage.length) {
-  console.log('Coverage is sufficient');
+  console.log('\u001b[32m ✨ Great test coverage \u001b[0m');
   process.exit(0);
 } else {
-  console.error('Coverage is insufficient');
+  console.error('\u001b[31m Coverage is insufficient  \u001b[0m');
   process.exit(1);
 }
