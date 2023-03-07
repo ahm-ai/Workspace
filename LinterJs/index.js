7676c176f4953c2d979c841964eab46be7b06b9a
ahm-ai <user@gmail.com>
2023-03-06 21:57:15 -0600

    M

--- LinterJs/index.js
+++ LinterJs/index.js
@@ -3,25 +3,14 @@ function sum(a, b) {
   return a + b;
 }
 
-
 function sum2(a, b) {
-  return a + b; 
+  return a + b;
 }
-
 
 function sum3(a, b) {
   return a + b;
 }
 
+const x = () => 1;
 
 module.exports = sum;
-
-
-
-
-
-
-
-
-
-
