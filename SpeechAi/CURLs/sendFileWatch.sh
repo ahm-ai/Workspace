62bf50d855b3596ab2c877f04db32a1c7bf0490f
Angelhmm <user@gmail.com>
2024-01-24 09:59:08 -0600

    M

--- SpeechAi/CURLs/sendFileWatch.sh
+++
@@ -1,5 +0,0 @@
-
-value1="/Users/home/Downloads/2023-01-10_21-15"
-
-curl -X POST http://localhost:8080/watch \
--H "Content-Type: application/json" -d "{\"folderName\":\"$value1\"}"
--- SpeechAi/ffmpegCommands/showDevices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-function showDevices(){
-    echo "List of devices"  
-    ffmpeg -hide_banner -f avfoundation -list_devices true -i ""
-}
-
-showDevices
