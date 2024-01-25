6bcc497878a1068e984da5e6068907df9d85962a
Angelhmm <user@gmail.com>
2024-01-25 13:01:25 -0600

    M

--- EsLint/NodeJs/node_lint.sh
+++ EsLint/NodeJs/node_lint.sh
@@ -2,12 +2,18 @@
 REPO_PATH=$1
 IGNORE_FOLDERS=$2
 
+# Save uncommitted changes to a stash
+git -C "$REPO_PATH" stash push -m "temp-stash-for-lint-script"
+
 # Switch to main branch and update
 git -C "$REPO_PATH" checkout main
 git -C "$REPO_PATH" pull
 
 # Switch back to your working branch
 git -C "$REPO_PATH" checkout -
+
+# Reapply stashed changes
+git -C "$REPO_PATH" stash pop
 
 # Get list of modified JavaScript and TypeScript files compared to main branch
 MODIFIED_FILES=$(git -C "$REPO_PATH" diff --name-only main...HEAD | grep -E '\.(js|ts|jsx|tsx)$')
@@ -25,14 +31,9 @@ for file in "${FILTERED_FILES[@]}"; do
     echo -e "\033[0;32m$file\033[0m"
 done
 
-
 # Run ESLint on these files
 for file in "${FILTERED_FILES[@]}"; do
     npx eslint "$REPO_PATH/$file" --config "./.eslintrc.js"
 done
-
-
-# Run ESLint on these files in one line
-# npx eslint $(printf "%s " "${FILTERED_FILES[@]/#/$REPO_PATH/}") --config "./.eslintrc.js"
 
 # RUN AS ./react_lint.sh /path/to/your/repo 'node_modules|dist'
