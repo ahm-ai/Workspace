6e1a55b497a9e41ac36446c285ddda993b883966
ah <ah@example.com>
2024-01-29 23:37:25 -0600

    Update

--- EsLint/NodeJs/node_lint.sh
+++ EsLint/NodeJs/node_lint.sh
@@ -1,6 +1,7 @@
 # Path to your Git repository
-REPO_PATH=$1
+REPO_PATH=~/Downloads/node-project
 IGNORE_FOLDERS=$2
+FILES_FOLDER="files"
 
 # Save uncommitted changes to a stash
 git -C "$REPO_PATH" stash push -m "temp-stash-for-lint-script"
@@ -16,7 +17,7 @@ git -C "$REPO_PATH" checkout -
 git -C "$REPO_PATH" stash pop
 
 # Get list of modified JavaScript and TypeScript files compared to main branch
-MODIFIED_FILES=$(git -C "$REPO_PATH" diff --name-only main...HEAD | grep -E '\.(js|ts|jsx|tsx)$')
+MODIFIED_FILES=$(git -C "$REPO_PATH" diff --name-only main...HEAD |  grep -E '\.(js|ts|jsx|tsx)$')
 
 # Filter out files in ignored folders
 FILTERED_FILES=()
@@ -26,14 +27,23 @@ for file in $MODIFIED_FILES; do
     fi
 done
 
-# Echo in green color
+# Create files folder and copy modified files
 for file in "${FILTERED_FILES[@]}"; do
-    echo -e "\033[0;32m$file\033[0m"
+    # Extract directory path from the file path
+    dir_path=$(dirname "$FILES_FOLDER/$file")
+
+    # Create directory if it doesn't exist
+    mkdir -p "$dir_path"
+
+    # Copy file to the files folder, maintaining directory structure
+    cp "$REPO_PATH/$file" "$FILES_FOLDER/$file"
 done
 
-# Run ESLint on these files
+# Run ESLint on these files and move them back
 for file in "${FILTERED_FILES[@]}"; do
-    npx eslint "$REPO_PATH/$file" --config "./.eslintrc.js"
+    npx eslint --fix "$FILES_FOLDER/$file" --config "./.eslintrc.json"
+    # Move the linted file back to its original location
+    mv "$FILES_FOLDER/$file" "$REPO_PATH/$file"
 done
 
 # RUN AS ./react_lint.sh /path/to/your/repo 'node_modules|dist'
