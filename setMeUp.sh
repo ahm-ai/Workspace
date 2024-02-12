78bc00cf8fdb3384d54d384163e689ea8ca18af3
ahh <user@gmail.com>
2024-02-11 18:51:37 -0600

    M

--- setMeUp.sh
+++ setMeUp.sh
@@ -1,2 +1,5 @@
-cd ./Git && sh enableAliases.sh
-cd ./VSC && sh enableSnippets.sh
+cd ./Git && sh enableAliases.sh && cd ../
+cd ./VSC && sh enableSnippets.sh && cd ../
+cd ./VSC && sh enableSettings.json.sh && cd ../
+
+echo "\033[32m Complete\033[0m"
