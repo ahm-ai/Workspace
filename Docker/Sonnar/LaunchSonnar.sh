d07bed4a4a25901e3e2ebc3b52d2aba9d5bb3dd7
ahh <user@gmail.com>
2024-07-21 15:14:00 -0500

    Update

--- Docker/Sonnar/LaunchSonnar.sh
+++ Docker/Sonnar/LaunchSonnar.sh
@@ -9,7 +9,12 @@ brew install sonar-scanner
 sonar-scanner -X \
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.login=admin \
-  -Dsonar.password=<PASSWORD> \
+  -Dsonar.password=$SONNAR_PASS \
   -Dsonar.projectKey=project \
   -Dsonar.projectName=project \
   -Dsonar.sources=.
+
+
+curl -u admin:$SONNAR_PASS "http://localhost:9000/api/issues/search?componentKeys=project" | \
+jq -r '.issues[] | "\(.component):\(.line):\(.message) [\(.severity)]"' | \
+reviewdog -f=checkstyle -reporter=github-pr-review
