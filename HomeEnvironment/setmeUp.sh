76a18d81954bf84212d89130525c55a114f65442
Ah <ah@Mac.lan>
2025-03-20 20:27:50 -0500

    update

---
+++ HomeEnvironment/setmeUp.sh
@@ -0,0 +1,7 @@
+
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+brew install fzf
